51e0712b521d9cb2ad79530d91b904193b1ce505
JPaulo Morais <joaopaulom19@hotmail.com>
2020-09-10 16:24:14 -0300

    falta alguns pra por no .h e no .cpp

--- Matrix_main.cpp
+++ Matrix_main.cpp
@@ -10,23 +10,23 @@ int main()
     Matrix Y;
     Matrix X(3,1), A(3,3), C(3,3);
     Matrix Z(3,2,7.0);
-    //Matrix W(in);
+    Matrix W(in);
     
     // operadores a serem implementados em sua classe:
     
-    /*Falta no .hA(2,1) = 10;*/                               // altera o valor de uma posição de A
+    /*Falta no .h*/ A(2,1) = 10;                               // altera o valor de uma posição de A
     C = A + A;                                  // Soma
     C -= A;                                     // Subtração       
     A = C - A;                                  // Subtração
     A += A;                                   // Soma
-    /*Falta no .hA = ~C;  */                                // A é igual a transposta de C
+    /*Falta no .h*/ A = ~C;                                  // A é igual a transposta de C
     X *= 2;                                   // multiplicação por uma constante
     C = A*X;                                         // multiplicação de matrizes
     C *= X;                                            // multiplicação de matrizes
     if (A == C)  ;                               // verifica a igualdade entre A e C
     if(X != Y) ;                                 // verifica a desigualdade entre A e C
-    /*n sei se ta certo no .hcout << Z << endl;*/                        // impressão de matrizes
-    /*Falta no .hcin >> W; */                                       // leitura de dados para dentro da matriz Y 
+    /*n sei se ta certo no .h*/ cout << Z << endl;                        // impressão de matrizes
+    /*Falta no .h*/ cin >> W;                                        // leitura de dados para dentro da matriz Y 
   
     return 0;
 }
