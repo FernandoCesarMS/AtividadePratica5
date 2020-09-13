469362b39102ed5556b93f0c5049a826cebac5fc
Fernando Cesar <51773254+FernandoUFMG@users.noreply.github.com>
2020-09-13 00:26:49 -0300

    100% atualizado, eh ruim de aturar

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "matrix.hpp"
+#include "Matrix.hpp"
 
 // construtor default - cria uma matriz vazia com nRows = nCols = 0
-Matrix::Matrix(){
+Matrix::Matrix()
+{
     m = NULL;
     nRows = 0;
     nCols = 0;
@@ -12,89 +13,110 @@ Matrix::Matrix(){
 
 // construtor parametrico 1 - cria uma matriz com nRows  = rows, nCols = cols e
 // com todos os elementos iguais a 0.0 (double)
-Matrix::Matrix(int rows, int cols, const double &value){
+Matrix::Matrix(int rows, int cols, const double &value)
+{
     nRows = rows;
     nCols = cols;
     m = new double *[nRows];
-    for (int i = 0; i < nRows; i++){
+    for (int i = 0; i < nRows; i++)
+    {
         m[i] = new double[nCols];
-        for (int j = 0; j < nCols; j++){
+        for (int j = 0; j < nCols; j++)
+        {
             m[i][j] = 0.0;
         }
     }
 }
 
 // construtor parametrico 2 - cria uma matriz com os dados fornecidos pelo arquivo texto myFile.
-Matrix::Matrix(fstream &myFile){
+Matrix::Matrix(ifstream &myFile)
+{
     vector<string> texto;
-    myFile.open("teste.txt", ios::in);
-    for (int i = 0; !myFile.fail(); i++){ // Guardando as informações do arquivo num vector de string
+    for (int i = 0; !myFile.fail(); i++)
+    { // Guardando as informações do arquivo num vector de string
         string str;
         getline(myFile, str);
         texto.push_back(str);
     }
-    this->nRows = texto.size()-1;
+    this->nRows = texto.size() - 1;
     m = new double *[this->nRows];
-    for (int i = 0; i < texto.size() - 1; i++){
+    for (int i = 0; i < texto.size() - 1; i++)
+    {
         vector<double> matriz;
-        for (int j = 0; j < texto[i].size(); j++){ // transformando os caracteres em int
+        for (int j = 0; j < texto[i].size(); j++)
+        { // transformando os caracteres em int
             int num;
-            if (((int)texto[i][j] >= 48) && ((int)texto[i][j] <= 57)){
+            if (((int)texto[i][j] >= 48) && ((int)texto[i][j] <= 57))
+            {
                 num = texto[i][j] - '0';
                 matriz.push_back(num);
             }
         }
         m[i] = new double[matriz.size()];
-        for (int j = 0; j < matriz.size(); j++){ // atribuindo os numeros a matrix
+        for (int j = 0; j < matriz.size(); j++)
+        { // atribuindo os numeros a matrix
             this->m[i][j] = matriz[j];
         }
-        this-> nCols = matriz.size();
+        this->nCols = matriz.size();
     }
     myFile.close();
 }
 
 // construtor de copia
-Matrix::Matrix(const Matrix &that){
+Matrix::Matrix(const Matrix &that)
+{
     this->nRows = that.nRows;
     this->nCols = that.nCols;
-    for (int i = 0; i < nRows; i++){
-        for (int j = 0; j < nCols; j++){
+    for (int i = 0; i < nRows; i++)
+    {
+        for (int j = 0; j < nCols; j++)
+        {
             this->m[i][j] = that.m[i][j];
         }
     }
 }
 
 // destrutor
-Matrix::~Matrix(){
-    for (int i; i < nRows; i++){
+Matrix::~Matrix()
+{
+    for (int i; i < nRows; i++)
+    {
         delete[] m[nCols];
         delete[] m;
     }
 }
 // obtem o numero de linhas
-int Matrix::getRows() const{
+int Matrix::getRows() const
+{
     return nRows;
 }
 // obtem o numero de colunas
-int Matrix::getCols() const{
+int Matrix::getCols() const
+{
     return nCols;
 }
 
 // obtem um elemento específico na posição (row,col). Obs: deve checar consistencia
-double Matrix::get(int row, int col) const{
-    if (row <= (this->nRows - 1) && col <= (this->nCols - 1)){
+double Matrix::get(int row, int col) const
+{
+    if (row <= (this->nRows - 1) && col <= (this->nCols - 1))
+    {
         return this->m[row][col];
     }
-    else{
+    else
+    {
         cout << "Não foi encontrado esse elemento!" << endl;
         return 0.0;
     }
 }
 
 // imprime o conteudo da matriz
-void Matrix::print() const{
-    for (int i = 0; i < nRows; i++){
-        for (int j = 0; j < nCols; j++){
+void Matrix::print() const
+{
+    for (int i = 0; i < nRows; i++)
+    {
+        for (int j = 0; j < nCols; j++)
+        {
             std::cout << m[i][j] << " ";
         }
         std::cout << "\n";
@@ -102,29 +124,43 @@ void Matrix::print() const{
 }
 
 // faz com que a matriz torne-se uma matriz identidade
-void Matrix::unit(){
-    if (nCols == nRows){
-        for (int i = 0; i <= nRows; i++){
-            for (int j = 0; j <= nCols; j++){
-                if (j == i){
-                    m[i][j] = 1;
+void Matrix::unit()
+{
+    if (nCols == nRows)
+    {
+
+        for (int i = 0; i < nRows; i++)
+        {
+
+            for (int j = 0; j < nCols; j++)
+            {
+
+                if (j == i)
+                {
+                    m[i][j] = 1.0;
                 }
-                else{
-                    m[i][j] = 0;
+                else
+                {
+
+                    m[i][j] = 0.0;
                 }
             }
         }
     }
-    else{
+    else
+    {
         std::cout << "Essa matriz nao pode ser transformada em identidade";
     }
 }
 
 // faz com que a matriz torne-se uma matriz nula
-void Matrix::zeros(){
-    for (int i = 0; i <= nRows; i++){
-        for (int j = 0; j <= nCols; j++){
-            m[i][j] = 0;
+void Matrix::zeros()
+{
+    for (int i = 0; i < nRows; i++)
+    {
+        for (int j = 0; j < nCols; j++)
+        {
+            m[i][j] = 0.0;
         }
     }
 }
@@ -132,9 +168,11 @@ void Matrix::zeros(){
 // faz com que a matriz torne-se uma matriz cujos elementos sao iguais a 1
 void Matrix::ones()
 {
-    for (int i = 0; i <= nRows; i++){
-        for (int j = 0; j <= nCols; j++){
-            m[i][j] = 1.1;
+    for (int i = 0; i < nRows; i++)
+    {
+        for (int j = 0; j < nCols; j++)
+        {
+            m[i][j] = 1.0;
         }
     }
 }
@@ -143,143 +181,190 @@ void Matrix::ones()
 //metodos de sobrecarga de operadores
 
 //verifica igualdade entre as matrizes
-bool Matrix::operator == (const Matrix& that) const{
-    if(this->nRows == that.nRows){
-        if(this->nCols == that.nCols){
-            for(int i = 0; i < this->nRows; i++){
-                for(int j = 0; j < this->nCols; j++){
-                    if(this->m[i][j] != that.m[i][j]){
+bool Matrix::operator==(const Matrix &that) const
+{
+    if (this->nRows == that.nRows)
+    {
+        if (this->nCols == that.nCols)
+        {
+            for (int i = 0; i < this->nRows; i++)
+            {
+                for (int j = 0; j < this->nCols; j++)
+                {
+                    if (this->m[i][j] != that.m[i][j])
+                    {
                         return 0;
                     }
                 }
             }
-        return 1;
+            return 1;
         }
-    return 0;
+        return 0;
     }
     return 0;
-}; 
+};
 
 //verifica desigualdade
-bool Matrix::operator != (const Matrix& that) const{
-    if(this->nRows == that.nRows){
-        if(this->nCols == that.nCols){
-            for(int i = 0; i < this->nRows; i++){
-                for(int j = 0; j < this->nCols; j++){
-                    if(this->m[i][j] != that.m[i][j]){
+bool Matrix::operator!=(const Matrix &that) const
+{
+    if (this->nRows == that.nRows)
+    {
+        if (this->nCols == that.nCols)
+        {
+            for (int i = 0; i < this->nRows; i++)
+            {
+                for (int j = 0; j < this->nCols; j++)
+                {
+                    if (this->m[i][j] != that.m[i][j])
+                    {
                         return 1;
                     }
                 }
             }
-        return 0;
+            return 0;
         }
-    return 1;
+        return 1;
     }
     return 1;
-}; 
+};
 
 //soma
-Matrix Matrix::operator + (const Matrix& that){
-    if(this->nRows == that.nRows){
-        if(this->nCols == that.nCols){
-            for(int i = 0; i < this->nRows; i++){
-                for(int j = 0; j < this->nCols; j++){
+Matrix Matrix::operator+(const Matrix &that)
+{
+    if (this->nRows == that.nRows)
+    {
+        if (this->nCols == that.nCols)
+        {
+            for (int i = 0; i < this->nRows; i++)
+            {
+                for (int j = 0; j < this->nCols; j++)
+                {
                     this->m[i][j] + that.m[i][j];
                 }
             }
             return *this;
         }
-        else{
-        std::cout << "Matrixes incompativeis para essa operacao"; 
-    }
-    }
-    else{
-        std::cout << "Matrixes incompativeis para essa operacao"; 
-    }
-}; 
-
-//soma
-Matrix Matrix::operator += (const Matrix& that){
-    if(this->nRows == that.nRows){
-        if(this->nCols == that.nCols){
-            for(int i = 0; i < this->nRows; i++){
-                for(int j = 0; j < this->nCols; j++){
-                    this->m[i][j] + that.m[i][j];
-                }
-            }
-            return *this;
+        else
+        {
+            std::cout << "Matrixes incompativeis para essa operacao";
         }
-        else{
-        std::cout << "Matrixes incompativeis para essa operacao" ;
     }
-    }
-    else{
-        std::cout << "Matrixes incompativeis para essa operacao" ;
-    }
-}; 
-
-//subtrai
-Matrix Matrix::operator - (const Matrix& that){
-    if(this->nRows == that.nRows){
-        if(this->nCols == that.nCols){
-            for(int i = 0; i < this->nRows; i++){
-                for(int j = 0; j < this->nCols; j++){
-                    this->m[i][j] - that.m[i][j];
-                }
-            }
-            return *this;
-        }
-        else{
-        std::cout << "Matrixes incompativeis para essa operacao" ;
-    }
-    }
-    else{
-        std::cout << "Matrixes incompativeis para essa operacao" ;
-    }
-}; 
-
-//subtrai
-Matrix Matrix::operator -= (const Matrix& that){
-    if(this->nRows == that.nRows){
-        if(this->nCols == that.nCols){
-            for(int i = 0; i < this->nRows; i++){
-                for(int j = 0; j < this->nCols; j++){
-                    this->m[i][j] - that.m[i][j];
-                }
-            }
-            return *this;
-        }
-        else{
+    else
+    {
         std::cout << "Matrixes incompativeis para essa operacao";
     }
+};
+
+//soma
+Matrix Matrix::operator+=(const Matrix &that)
+{
+    if (this->nRows == that.nRows)
+    {
+        if (this->nCols == that.nCols)
+        {
+            for (int i = 0; i < this->nRows; i++)
+            {
+                for (int j = 0; j < this->nCols; j++)
+                {
+                    this->m[i][j] + that.m[i][j];
+                }
+            }
+            return *this;
+        }
+        else
+        {
+            std::cout << "Matrixes incompativeis para essa operacao";
+        }
     }
-    else{
-        std::cout << "Matrixes incompativeis para essa operacao" ;
+    else
+    {
+        std::cout << "Matrixes incompativeis para essa operacao";
+    }
+};
+
+//subtrai
+Matrix Matrix::operator-(const Matrix &that)
+{
+    if (this->nRows == that.nRows)
+    {
+        if (this->nCols == that.nCols)
+        {
+            for (int i = 0; i < this->nRows; i++)
+            {
+                for (int j = 0; j < this->nCols; j++)
+                {
+                    this->m[i][j] - that.m[i][j];
+                }
+            }
+            return *this;
+        }
+        else
+        {
+            std::cout << "Matrixes incompativeis para essa operacao";
+        }
+    }
+    else
+    {
+        std::cout << "Matrixes incompativeis para essa operacao";
+    }
+};
+
+//subtrai
+Matrix Matrix::operator-=(const Matrix &that)
+{
+    if (this->nRows == that.nRows)
+    {
+        if (this->nCols == that.nCols)
+        {
+            for (int i = 0; i < this->nRows; i++)
+            {
+                for (int j = 0; j < this->nCols; j++)
+                {
+                    this->m[i][j] - that.m[i][j];
+                }
+            }
+            return *this;
+        }
+        else
+        {
+            std::cout << "Matrixes incompativeis para essa operacao";
+        }
+    }
+    else
+    {
+        std::cout << "Matrixes incompativeis para essa operacao";
     }
 };
 
 //multiplicação por C
-Matrix Matrix::operator * (int C){
-    for(int i = 0; i < this->nRows; i++){
-        for(int j = 0; j < this->nCols; j++){
-            this -> m[i][j] * C;
+Matrix Matrix::operator*(int C)
+{
+    for (int i = 0; i < this->nRows; i++)
+    {
+        for (int j = 0; j < this->nCols; j++)
+        {
+            this->m[i][j] * C;
         }
     }
     return *this;
 };
 
 //multiplicação por C
-Matrix Matrix::operator *= (int C){
-    for(int i = 0; i < this->nRows; i++){
-        for(int j = 0; j < this->nCols; j++){
-            this -> m[i][j] * C;
+Matrix Matrix::operator*=(int C)
+{
+    for (int i = 0; i < this->nRows; i++)
+    {
+        for (int j = 0; j < this->nCols; j++)
+        {
+            this->m[i][j] * C;
         }
     }
     return *this;
 };
 
-//multiplicação por matrix 
-Matrix Matrix::operator * (const Matrix& that){
+//multiplicação por matrix
+Matrix Matrix::operator*(const Matrix &that)
+{
     Matrix retorno;
     retorno.nCols = this->nRows;
     retorno.nRows = that.nCols;
@@ -300,9 +385,10 @@ Matrix Matrix::operator * (const Matrix& that){
 };
 
 //multiplicação por matrix
-Matrix Matrix::operator *= (const Matrix& that){
+Matrix Matrix::operator*=(const Matrix &that)
+{
     int x = this->getCols();
-    Matrix Aux(this->getRows(),this->getCols());
+    Matrix Aux(this->getRows(), this->getCols());
     this->nCols = that.getCols();
     Aux.m = new double *[this->nRows];
     if (x == that.getRows())
@@ -326,10 +412,11 @@ Matrix Matrix::operator *= (const Matrix& that){
         }
     }
     return Aux;
-}; 
+};
 
 //impressao da matrix
-void Matrix::operator << (const Matrix) const{
+void Matrix::operator<<(const Matrix) const
+{
 
     for (int i = 0; i < this->nRows; i++)
     {
@@ -339,22 +426,23 @@ void Matrix::operator << (const Matrix) const{
         }
         std::cout << "\n";
     }
-
-}; 
+};
 
 //transposta
-Matrix& Matrix::operator ~ (){
+Matrix &Matrix::operator~()
+{
 
     int auxRows = nCols;
     int auxCols = nRows;
-    Matrix aux(auxRows,auxCols);
+    Matrix aux(auxRows, auxCols);
     aux.m = new double *[auxRows];
-    for(int i = 0; i < auxRows; i++){
-        aux.m[i] = new double [auxCols];
-        for(int j = 0; j < auxCols; j++){
+    for (int i = 0; i < auxRows; i++)
+    {
+        aux.m[i] = new double[auxCols];
+        for (int j = 0; j < auxCols; j++)
+        {
             aux.m[i][j] = m[j][i];
-            return m[j][i];
         }
     }
-
-}; 
+    return aux;
+};
